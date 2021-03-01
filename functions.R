f3a4de67e609ec3da741bf30810d97150c443536
Zstar <zxlee@hku.hk>
2021-03-01 20:04:40 +0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,1 +1,29 @@
+# Nearby (nGene)
+get_nGene() <- function() {
+  # do something
+}
 
+# eQTL (eGene)
+get_eGene() <- function() {
+  # do something
+} 
+
+# Conformation (cGene)
+get_cGene() <- function() {
+  # do something
+} 
+
+# Function (fGene)
+get_fGene() <- function() {
+  # do something
+}
+
+# Phenotype (pGene)
+get_pGene() <- function() {
+  # do something
+}
+
+# Disease (dGene)
+get_dGene() <- function() {
+  # do something
+}
